b142781ca1e1e21896e61523c23287ddf21aa698
Emupedia <admin@emupedia.net>
2020-10-06 06:51:01 +0300

    More work

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<!--suppress ES6UnusedImports, JSUnresolvedVariable -->
+<!--suppress ES6UnusedImports, JSUnresolvedVariable, HtmlUnknownTarget -->
 <script>
 	import { Route, router } from 'tinro'
 	import EmuOS from './components/EmuOS.svelte'
@@ -27,5 +27,28 @@
 <Route path="/*">
 	<Route path="/"><EmuOS development={development} route={route} /></Route>
 	<Route path="/emuos/"><EmuOS development={development} route={route} /></Route>
-	<Route fallback>No page found</Route>
+	<Route fallback>
+		<table class="error">
+			<tr>
+				<td>
+					<div class="error-wrapper">
+						<h1>The page you are looking for is temporarily unavailable!</h1>
+						<h2>
+							Sorry, the page you are looking for is currently unavailable, please try again later.<br />
+							These errors are monitored and will be fixed as soon as posible.<br />
+							Thank you for understanding!
+						</h2>
+						<div class="error-container">
+							<video muted autoplay loop controls>
+								<source src="assets/videos/monkey.mp4" type="video/mp4">
+								<source src="assets/videos/monkey.ogg" type="video/ogg">
+								<img src="assets/videos/monkeys.jpg" alt="" />
+							</video>
+						</div>
+						<div class="error-caption">A team of highly trained monkeys has been dispatched to deal with this situation.</div>
+					</div>
+				</td>
+			</tr>
+		</table>
+	</Route>
 </Route>
