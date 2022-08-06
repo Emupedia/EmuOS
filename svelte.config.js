8c6d9d083e4243f8297b6176e4a82157e12fe6d5
Emupedia <admin@emupedia.net>
2022-08-06 22:07:06 +0300

    More work

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
+
+const isProd = process.env.NODE_ENV === 'production'
+const plugins = []
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +11,31 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		appDir: 'docs',
+		/*paths: {
+			base: '',
+		},*/
+		adapter: adapter({
+			pages: 'docs',
+			// assets: 'docs',
+			fallback: '404.html',
+			precompress: true
+		}),
+		files: {
+			/*assets: 'static',
+			hooks: 'src/hooks',
+			lib: 'src/lib',
+			params: 'src/params',
+			routes: 'src/routes',
+			serviceWorker: 'src/service-worker',*/
+			template: 'src/index.html'
+		},
+		/*prerender: {
+			default: false
+		},*/
+		serviceWorker: {
+			register: isProd
+		}
 	}
 }
 
