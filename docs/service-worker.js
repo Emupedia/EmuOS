fec73c3cf1b630c43832f9429c902307f269038c
Emupedia <admin@emupedia.net>
2022-08-07 01:54:13 +0300

    More work

---
+++ docs/service-worker.js
@@ -0,0 +1,149 @@
+const n = [
+  "/emuos/immutable/start-d5699e78.js",
+  "/emuos/immutable/pages/__layout.svelte-184a391e.js",
+  "/emuos/immutable/error.svelte-5ace1672.js",
+  "/emuos/immutable/pages/index.svelte-3b0a20c5.js",
+  "/emuos/immutable/chunks/index-0b20d084.js"
+], l = [
+  "/.nojekyll",
+  "/404.html",
+  "/CNAME",
+  "/apple-touch-icon.png",
+  "/assets/css/fonts.css",
+  "/assets/css/libraries/bootstrap-4.5.0.css",
+  "/assets/css/libraries/bootstrap-4.5.0.css.map",
+  "/assets/css/libraries/bootstrap-4.5.0.min.css",
+  "/assets/css/libraries/bootstrap-4.5.0.min.css.map",
+  "/assets/css/libraries/normalize-8.0.1.css",
+  "/assets/fonts/dosvga.eot",
+  "/assets/fonts/dosvga.svg",
+  "/assets/fonts/dosvga.ttf",
+  "/assets/fonts/dosvga.woff",
+  "/assets/fonts/dosvga.woff2",
+  "/assets/fonts/emuos.eot",
+  "/assets/fonts/emuos.svg",
+  "/assets/fonts/emuos.ttf",
+  "/assets/fonts/emuos.woff",
+  "/assets/fonts/emuos.woff2",
+  "/assets/images/clouds.png",
+  "/assets/images/logos/emupedia/empuedia-360-transparent.gif",
+  "/assets/images/logos/emupedia/emupedia-360-gray.gif",
+  "/assets/images/logos/emupedia/emupedia-logo-1024.png",
+  "/assets/images/logos/emupedia/emupedia-logo-114.png",
+  "/assets/images/logos/emupedia/emupedia-logo-128.png",
+  "/assets/images/logos/emupedia/emupedia-logo-144.png",
+  "/assets/images/logos/emupedia/emupedia-logo-152.png",
+  "/assets/images/logos/emupedia/emupedia-logo-16.png",
+  "/assets/images/logos/emupedia/emupedia-logo-168.png",
+  "/assets/images/logos/emupedia/emupedia-logo-180.png",
+  "/assets/images/logos/emupedia/emupedia-logo-192.png",
+  "/assets/images/logos/emupedia/emupedia-logo-24.png",
+  "/assets/images/logos/emupedia/emupedia-logo-256.png",
+  "/assets/images/logos/emupedia/emupedia-logo-32.png",
+  "/assets/images/logos/emupedia/emupedia-logo-48.png",
+  "/assets/images/logos/emupedia/emupedia-logo-512.png",
+  "/assets/images/logos/emupedia/emupedia-logo-58.png",
+  "/assets/images/logos/emupedia/emupedia-logo-64.png",
+  "/assets/images/logos/emupedia/emupedia-logo-72.png",
+  "/assets/images/logos/emupedia/emupedia-logo-96.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-1024.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-114.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-128.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-144.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-152.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-16.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-168.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-180.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-192.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-24.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-256.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-32.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-48.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-512.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-58.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-64.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-72.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas-96.png",
+  "/assets/images/logos/emupedia/emupedia-logo-xmas.png",
+  "/assets/images/logos/emupedia/emupedia-logo.ai",
+  "/assets/images/logos/emupedia/emupedia-logo.png",
+  "/assets/images/logos/emupedia/emupedia-logo.svg",
+  "/assets/js/libraries/babel-polyfill-7.10.1.js",
+  "/assets/js/libraries/babel-polyfill-7.10.1.min.js",
+  "/assets/js/libraries/closest.js",
+  "/assets/js/libraries/css-vars-4.0.1.js",
+  "/assets/js/libraries/fetch-3.0.0.js",
+  "/assets/js/libraries/promise-auto-4.2.8.js",
+  "/assets/js/libraries/promise-auto-4.2.8.js.map",
+  "/assets/js/libraries/promise-auto-4.2.8.min.js",
+  "/assets/js/libraries/promise-auto-4.2.8.min.js.map",
+  "/assets/js/libraries/remove.js",
+  "/assets/js/libraries/smoothscroll.js",
+  "/assets/js/libraries/system.js",
+  "/assets/js/main.min.js",
+  "/assets/js/main.min.js.map",
+  "/assets/sounds/error.mp3",
+  "/assets/sounds/hdd.mp3",
+  "/assets/sounds/post.mp3",
+  "/assets/themes/bios/award-logo.png",
+  "/assets/themes/bios/epa-logo.png",
+  "/assets/themes/boot/emuos-95/emuos-95-transparent.png",
+  "/assets/themes/boot/emuos-95/emuos-95.png",
+  "/assets/themes/boot/emuos-98/background.png",
+  "/assets/themes/boot/emuos-98/emuos-98-transparent.png",
+  "/assets/themes/boot/emuos-98/emuos-98.png",
+  "/assets/themes/boot/emuos-me/emuos-me-transparent.png",
+  "/assets/themes/boot/emuos-me/emuos-me.png",
+  "/assets/themes/boot/windows-3/win31.png",
+  "/assets/themes/boot/windows-3/win31nt.png",
+  "/assets/themes/boot/windows-7/boot-transparent.png",
+  "/assets/themes/boot/windows-95/background.png",
+  "/assets/themes/boot/windows-95/windows-95-remastered.png",
+  "/assets/themes/boot/windows-95/windows-95-transparent.png",
+  "/assets/themes/boot/windows-98/background.png",
+  "/assets/themes/boot/windows-98/windows-98-remastered.png",
+  "/assets/themes/boot/windows-98/windows-98-transparent.png",
+  "/assets/themes/boot/windows-me/windows-me-remastered.png",
+  "/assets/themes/boot/windows-me/windows-me-transparent.png",
+  "/assets/videos/monkey.jpg",
+  "/assets/videos/monkey.mp4",
+  "/assets/videos/monkey.ogg",
+  "/favicon.ico",
+  "/icons.html",
+  "/indexold.html",
+  "/manifest.webmanifest"
+], i = "1659826428802", m = `cache${i}`, g = n.concat(l), d = new Set(g);
+self.addEventListener("install", (s) => {
+  s.waitUntil(
+    caches.open(m).then((e) => e.addAll(g)).then(() => {
+      self.skipWaiting();
+    })
+  );
+});
+self.addEventListener("activate", (s) => {
+  s.waitUntil(
+    caches.keys().then(async (e) => {
+      for (const a of e)
+        a !== m && await caches.delete(a);
+      await self.clients.claim();
+    })
+  );
+});
+async function u(s) {
+  const e = await caches.open(`offline${i}`);
+  try {
+    const a = await fetch(s);
+    return await e.put(s, a.clone()), a;
+  } catch (a) {
+    const o = await e.match(s);
+    if (o)
+      return o;
+    throw a;
+  }
+}
+self.addEventListener("fetch", (s) => {
+  if (s.request.method !== "GET" || s.request.headers.has("range"))
+    return;
+  const e = new URL(s.request.url), a = e.protocol.startsWith("http"), o = e.hostname === self.location.hostname && e.port !== self.location.port, t = e.host === self.location.host && d.has(e.pathname), p = s.request.cache === "only-if-cached" && !t;
+  a && !o && !p && s.respondWith((async () => t && await caches.match(s.request) || u(s.request))());
+});
