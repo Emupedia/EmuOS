274299074891df67cc696dea6ec3e201b55543ba
Emupedia <admin@emupedia.net>
2022-08-23 12:43:22 +0300

    More work

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 const p = [
-  "/emuos/immutable/start-257536f2.js",
+  "/emuos/immutable/start-501a3215.js",
   "/emuos/immutable/pages/__layout.svelte-1496ce17.js",
   "/emuos/immutable/assets/__layout-7b4c4813.css",
   "/emuos/immutable/pages/__error.svelte-da1f0c2d.js",
@@ -223,7 +223,7 @@ const p = [
   "/favicon.ico",
   "/icons.html",
   "/manifest.webmanifest"
-], i = "1661247636608", r = `cache${i}`, m = p.concat(l.filter((s) => !s.startsWith("/_config.yml") && !s.startsWith("/CNAME") && !s.startsWith("/manifest.json"))), d = new Set(m);
+], i = "1661247785887", r = `cache${i}`, m = p.concat(l.filter((s) => !s.startsWith("/_config.yml") && !s.startsWith("/CNAME") && !s.startsWith("/manifest.json"))), d = new Set(m);
 self.addEventListener("install", (s) => {
   try {
     s.waitUntil(
