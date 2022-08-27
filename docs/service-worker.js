a384bb0bd2d292526c5631e44c22e5a525ad55e5
Emupedia <admin@emupedia.net>
2022-08-27 16:51:21 +0300

    More work

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,14 +1,14 @@
 const p = [
-  "/emuos/immutable/start-ef8135f0.js",
+  "/emuos/immutable/start-89f5b033.js",
   "/emuos/immutable/components/pages/_layout.svelte-228c4c71.js",
   "/emuos/immutable/assets/+layout-7c802733.css",
-  "/emuos/immutable/components/pages/_error.svelte-a032ebca.js",
+  "/emuos/immutable/components/pages/_error.svelte-37e8afd9.js",
   "/emuos/immutable/components/pages/_page.svelte-aee2d4b8.js",
   "/emuos/immutable/assets/+page-c572d587.css",
   "/emuos/immutable/components/pages/test/_page.svelte-ab6eb307.js",
   "/emuos/immutable/modules/pages/_layout.js-eff2ad23.js",
   "/emuos/immutable/modules/pages/_page.js-68bcb107.js",
-  "/emuos/immutable/chunks/singletons-72b3474f.js",
+  "/emuos/immutable/chunks/singletons-48c3db1b.js",
   "/emuos/immutable/chunks/index-4a5c5fd9.js",
   "/emuos/immutable/chunks/index-d050104e.js",
   "/emuos/immutable/chunks/variables-055bf2ca.js",
@@ -16,7 +16,7 @@ const p = [
   "/emuos/immutable/chunks/db-c776e8ef.js",
   "/emuos/immutable/chunks/_page-11d1adbc.js",
   "/emuos/immutable/chunks/0-40fc1566.js",
-  "/emuos/immutable/chunks/1-f52e7074.js",
+  "/emuos/immutable/chunks/1-231b19b1.js",
   "/emuos/immutable/chunks/2-deec83a6.js",
   "/emuos/immutable/chunks/3-4802a4c7.js"
 ], l = [
@@ -235,7 +235,7 @@ const p = [
   "/favicon.ico",
   "/icons.html",
   "/manifest.webmanifest"
-], i = "1661608224460", m = `cache${i}`, r = p.concat(l.filter((s) => !s.startsWith("/.nojekyll") && !s.startsWith("/_config.yml") && !s.startsWith("/CNAME") && !s.startsWith("/vite-manifest.json"))), u = new Set(r);
+], i = "1661608263128", m = `cache${i}`, r = p.concat(l.filter((s) => !s.startsWith("/.nojekyll") && !s.startsWith("/_config.yml") && !s.startsWith("/CNAME") && !s.startsWith("/vite-manifest.json"))), u = new Set(r);
 self.addEventListener("install", (s) => {
   try {
     s.waitUntil(
